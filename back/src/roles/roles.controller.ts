90e5aa8f96213c6d5737984e01bb18a74e11d479
Going <goinghiggvp@gmail.com>
2023-04-08 17:57:59 +0300

    setup controller role

--- back/src/roles/roles.controller.ts
+++ back/src/roles/roles.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RolesAuth } from './decorators/roles.decorator';
+import { Role } from './roles.enum';
 
-@Controller()
+@Controller('role')
 export class RolesController {
     
+    @RolesAuth(Role.USER)
+    @Get()
+    test() {
+        return 'you are admin'
+    }
 }
