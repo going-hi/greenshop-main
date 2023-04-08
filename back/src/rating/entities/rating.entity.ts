703d1b877972f1ff87c243f58d623cfdb065ed31
Going <goinghiggvp@gmail.com>
2023-04-08 23:35:23 +0300

    create rating entity

---
+++ back/src/rating/entities/rating.entity.ts
@@ -0,0 +1,19 @@
+import { UserEntity } from "src/user/entities/user.entity";
+import { BaseEntity, Column, Entity, ManyToOne } from "typeorm";
+import { IsInt, Min, Max } from "class-validator";
+import { ProductEntity } from "src/product/entities/product.entity";
+
+@Entity('rating')
+export class RatingEntity extends BaseEntity {
+    @Column()
+    @IsInt()
+    @Min(0)
+    @Max(5)
+    rating: number
+
+    @ManyToOne(() => UserEntity)
+    user: UserEntity
+
+    @ManyToOne(() => ProductEntity)
+    product: ProductEntity
+}
