88f16dd8b78f46617319e8f49fcd05ba3d886941
Going <goinghiggvp@gmail.com>
2023-04-08 18:09:34 +0300

    remove bag

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -19,7 +19,7 @@ import { UserModule } from './user/user.module';
     AuthModule,
     UserModule,
   ],
-  controllers: [RolesController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
