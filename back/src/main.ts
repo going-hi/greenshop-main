3873112d92af8843bd9f1ca0df144fbec2811838
Going <goinghiggvp@gmail.com>
2023-04-09 14:17:42 +0300

    cors + setFlobalPRefix

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,8 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
+  app.setGlobalPrefix('api')
+  app.enableCors()
   await app.listen(3100);
 }
 
