0b38ce196ca7870fd5dbb3e347c9b6fa2e46e48b
Going <goinghiggvp@gmail.com>
2023-04-10 16:46:55 +0300

    add method user get by id

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>
+    ) {}
+
+    async getUserById(id: number) {
+        const user = await this.userRepository.findOne({
+            where: {id},
+        })
+        return user
+    }
+}
