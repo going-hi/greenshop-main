cb018e7d249c866dba4b0a25efa52471230125d3
Going <goinghiggvp@gmail.com>
2023-04-10 19:24:43 +0300

    add role business logic

--- back/src/roles/roles.service.ts
+++ back/src/roles/roles.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { ActionRoleDto } from './dto/action-role.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class RolesService {
-    
+    constructor(
+        private readonly userService: UserService
+    ) {}
+
+   async setRoleUser({role, userId}: ActionRoleDto) {
+        const user = await this.userService.setRoleUser(userId, role)
+        if(!user) throw new ForbiddenException()
+
+        return
+   } 
 }
