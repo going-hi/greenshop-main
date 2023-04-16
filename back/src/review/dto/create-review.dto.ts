3bc59c2417a5f55563efbbcb2fb031edf8d0cc14
Going <goinghiggvp@gmail.com>
2023-04-16 19:23:00 +0300

    update review doc swagger

--- back/src/review/dto/create-review.dto.ts
+++ back/src/review/dto/create-review.dto.ts
@@ -2,20 +2,13 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsNotEmpty, IsOptional, Max, Min } from "class-validator";
 
 export class CreateReviewDto {
-
-    @ApiProperty()
     @IsInt()
     productId: number
 
-    @ApiProperty({required: false})
     @IsOptional() 
     @IsNotEmpty()
     text: string
 
-    @ApiProperty({
-        minimum: 1,
-        maximum: 5
-    })
     @Max(5)
     @Min(1)
     rating: number
