bebfec89826d2e3c12f129e3fc2b55d992b97687
Going <goinghiggvp@gmail.com>
2023-04-16 19:22:50 +0300

    update category doc swagger

--- back/src/category/dto/create-category.dto.ts
+++ back/src/category/dto/create-category.dto.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateCategoryDto {
-    @ApiProperty({description: 'Название категории'})
     @IsString()
     name: string
 }
--- back/src/category/dto/update-category.dto.ts
+++ back/src/category/dto/update-category.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
-import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
-    @ApiProperty()
-    name?: string;
-}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
