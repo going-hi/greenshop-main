c71cc2b730b95eb47972529bce5e842a4fbfb7cd
Going <goinghiggvp@gmail.com>
2023-04-20 18:42:28 +0300

    add helmet

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.setGlobalPrefix('api')
   app.enableCors()
-
+  app.use(helmet())
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Api GreenShop')
